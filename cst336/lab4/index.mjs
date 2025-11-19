d8c93d8d23766f785f3466d7f3fe30876c1e46df
JianMitchell <jimitchell@csumb.edu>
2025-11-18 19:43:52 -0800

    this is for the express app setup - 3

--- cst336/lab4/index.mjs
+++ cst336/lab4/index.mjs
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello Express app!')
 });
 
-app.listen(3000, () => {
-  console.log('server started');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server started`);
 });
